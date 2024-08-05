65f66e0f15ed2d4886ce356e74ef0cd9751f1515
Lucas-M-Bruno <165830297+Lucas-M-Bruno@users.noreply.github.com>
2024-08-05 14:55:06 -0300

    Add files via upload
    js do site

---
+++ testee.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('#abrir-form').click(function(){
+    $('.formulario').slideToggle();
+  });
+
+  $('.astros').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false
+  });
+})
